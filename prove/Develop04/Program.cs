4dc842d42ffbfd25bb05e9a6c99e05014460198c
markdavidc <davidhawk2001@gmail.com>
2025-03-01 23:57:53 -0700

    of

--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -1,5 +1,5 @@
 using System;
-// as a stretch, I tried to keep a log fo how many times activities were performed. 
+// as a stretch, I tried to keep a log of how many times activities were performed. 
 class Program
 {
     static void Main(string[] args)
